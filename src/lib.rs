bf098aa1136687cef20ca22adad614474765e16d
noah-clements <noahclements@gmail.com>
2023-03-15 03:52:59 -0400

    took out enum and external crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,25 @@
-extern crate int_enum;
-
-use int_enum::IntEnum;
-
-#[repr(u8)]
-#[derive(Debug, PartialEq, Eq, IntEnum, Copy, Clone)]
-pub enum Actions {
-    Wink = 0b00001,
-    DoubleBlink = 0b00010,
-    CloseYourEyes = 0b00100,
-    Jump = 0b01000,
-    Reverse = 0b10000,
-}
 
 pub fn actions(n: u8) -> Vec<&'static str> {
+    let wink: u8 = 0b00001;
+    let double_blink: u8 = 0b00010;
+    let close_your_eyes: u8 = 0b00100;
+    let jump: u8 = 0b01000;
+    let reverse: u8 = 0b10000;
+
     let mut v: Vec<&'static str> = Vec::new();
-    if n & Actions::Wink.int_value() == Actions::Wink.int_value() {
+    if n & wink == wink {
         v.push("wink");
     }
-    if n & Actions::DoubleBlink.int_value() == Actions::DoubleBlink.int_value() {
+    if n & double_blink == double_blink {
         v.push("double blink");
     }
-    if n & Actions::CloseYourEyes.int_value() == Actions::CloseYourEyes.int_value() {
+    if n & close_your_eyes == close_your_eyes {
         v.push("close your eyes");
     }
-    if n & Actions::Jump.int_value() == Actions::Jump.int_value() {
+    if n & jump == jump {
         v.push("jump");
     }
-    if n & Actions::Reverse.int_value() == Actions::Reverse.int_value() {
+    if n & reverse == reverse {
         v.reverse();
     }
 
